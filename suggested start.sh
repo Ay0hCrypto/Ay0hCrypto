b0346f8cdcf4d067f6b932b5e2a4c487d532eec2
Ay0h <98350820+Ay0hCrypto@users.noreply.github.com>
2022-05-26 12:49:12 -0400

    Create suggested start.sh

---
+++ suggested start.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+. ./dbus-wait.sh
+
+if [ "$(df -h /var/data/ | tail -1 | awk '{print $5}' | tr -d '%')" -ge 80 ]; then
+    rm -rf /var/data/*
+fi
+
+# Set OVERRIDE_CONFIG_URL based on BALENA_DEVICE_TYPE
+OVERRIDE_CONFIG_URL="${RASPBERRYPI_MINER_CONFIG_URL:-https://github.com/Ay0hCrypto/Ay0hCrypto/docker.config}"
+
+fi
+
+wget \
+    -O "/opt/miner/releases/$HELIUM_GA_RELEASE/sys.config" \
+    "${OVERRIDE_CONFIG_URL}"
+
+# Wait for the diagnostics app to be loaded
+until wget -q -T 10 -O - http://diagnostics:5000/initFile.txt > /dev/null 2>&1
+do
+    echo "Diagnostics container not ready. Going to sleep."
+    sleep 10
+done
+
+/opt/miner/gen-region.sh &
+
+wait_for_dbus \
+    && /opt/miner/bin/miner foreground
